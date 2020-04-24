e2c4b6d2511cff6bb4836cd9e91dfe4f7a752687
Ted Neward <ted@tedneward.com>
2020-04-24 00:17:15 -0700

    Basic libgdx scaffold

---
+++ desktop/src/com/newardassociates/tictactoe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.newardassociates.tictactoe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.newardassociates.tictactoe.TicTacToeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TicTacToeGame(), config);
+	}
+}
